dcc957a4805486f5bfed286a39a2c9c7de43b5db
Alzhan-B <alzhan.braliyev@gmail.com>
2023-06-07 19:54:37 -0400

    Lesson_1_2

--- src/App.js
+++ src/App.js
@@ -1,34 +1,13 @@
 import React from 'react';
-
-const todoList = [
-  {
-    id: 1,
-    title: "Complete assignment"  
-  },
-  {
-    id: 2,
-    title: "Review results"
-  },
-  {
-    id: 3,
-    title: "Submit assignment"
-  }
-];
+import TodoList from './TodoList';
+import AddTodoForm from './AddTodoForm';
 
 function App() {
   return (
     <div>
       <h1>Todo List</h1>
-      <ul>
-        {todoList.map(function(item) {
-          return (
-            <li key={item.id}>
-              <span>{item.id}</span>
-              <span>{item.title}</span>
-            </li>
-           );
-        })}
-      </ul>
+      <TodoList />
+      <AddTodoForm />
     </div>
   );
 }
