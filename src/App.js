bd2a0110370b6249b58dbf99382c8141fbf1d531
Alzhan-B <alzhan.braliyev@gmail.com>
2023-06-18 22:04:43 -0400

    Lesson_1_3

--- src/App.js
+++ src/App.js
@@ -3,12 +3,16 @@ import TodoList from './TodoList';
 import AddTodoForm from './AddTodoForm';
 
 function App() {
+ 
+ const [newTodo, setNewTodo] = React.useState('');
+
   return (
-    <div>
+    <>
       <h1>Todo List</h1>
       <TodoList />
-      <AddTodoForm />
-    </div>
+      <AddTodoForm onAddTodo={setNewTodo} />
+      <p>{newTodo}</p>
+    </>
   );
 }
 
